0ab8b2345544a0e4e65cf504af08f63a2e31098b
fumitrial8 <fumitrial8@gmail.com>
2019-11-24 00:49:05 +0900

    complete

--- app.py
+++ app.py
@@ -1,24 +1,77 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 import os
-from flask import Flask, render_template
 from flask_socketio import SocketIO
 from pytz import timezone
+from flask import Flask,render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import keras, sys
+from keras.models import Sequential, load_model
+from PIL import Image
+import numpy as np
+os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
+
+classes = [ "himeno", "naoya"]
+num_classes = len(classes)
+image_size = 50
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 socketio = SocketIO(app)
 thread = None
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
-    tz = timezone('Asia/Tokyo')
-    now = datetime.now(tz)
-    return render_template(
-            'index.html',
-            hour=__make_clock_img_path('hours', now.hour),
-            minute=__make_clock_img_path('minutes', now.minute)
-            )
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('ファイルがありません')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('ファイルがありません')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            
+            model = load_model("./animal_cnn_aug.h5")
+
+            image = Image.open(filepath)
+            image = image.convert("RGB")
+            image = image.resize((image_size, image_size))
+            data = np.asarray(image)
+            X = []
+            X.append(data)
+            X = np.array(X)
+
+            result = model.predict([X])[0]
+            predicted = result.argmax()
+            percentage = int(result[predicted] * 100)
+
+            return classes[predicted] + "確率:"+ str(percentage) + " %"
+            # return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <html>
+    <head>
+    <meta charset='UTF-8'>
+    <title>ファイルをアップロードして判定しよう</title></head>
+    <body>
+    <h1>ファイルをアップロードして判定しよう！</h1>
+    <form method = post enctype = multipart/form-data>
+    <p><input type = file name = file></p>
+    <input type = submit value = Upload>
+    </form>
+    </body>
+    </html>
+    '''
 
 
 @socketio.on('connect', namespace='/socket')
@@ -26,32 +79,6 @@ def connect():
     global thread
     if thread is None:
         thread = socketio.start_background_task(target=background_thread)
-
-
-def background_thread():
-    while True:
-        tz = timezone('Asia/Tokyo')
-        before = datetime.now(tz)
-        socketio.sleep(1)
-        after = datetime.now(tz)
-
-        if before.hour != after.hour:
-            path = __make_clock_img_path('hours', after.hour)
-            socketio.emit('change hour',
-                          {'img_path': os.path.join('/static', path)},
-                          namespace='/socket')
-
-        if before.minute != after.minute:
-            path = __make_clock_img_path('minutes', after.minute)
-            socketio.emit('change minute',
-                          {'img_path': os.path.join('/static', path)},
-                          namespace='/socket')
-
-
-def __make_clock_img_path(div, num):
-    num_text = '%02d' % num
-    return "img/%s/%s.png" % (div, num_text)
-
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', debug=True)
